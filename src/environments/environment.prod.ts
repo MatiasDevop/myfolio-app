f59171baa909e0e41c8a08a506b55da7f72435d1
Nestor Matias <solomatias321@gmail.com>
2022-04-27 16:04:37 +0200

    fixing angular issues and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,13 @@ export const environment = {
   token:'abcProd',
   port:8080,
   envName:'prod',
+  firebaseConfig: {
+    apiKey: "AIzaSyAvQkJ1sAtquc2dP5Ll6wKMepJkN9_2oHc",
+    authDomain: "myngsite.firebaseapp.com",
+    projectId: "myngsite",
+    storageBucket: "myngsite.appspot.com",
+    messagingSenderId: "1005158249226",
+    appId: "1:1005158249226:web:d69c2608da15eab7bcc0f3",
+    measurementId: "G-EHNRMT06C3"
+  }
 };
